df7fe62bcc36a790f2b0e89322226ec7b366c131
SeleznevAS <seleznevanatserg@yahoo.com>
2022-12-11 21:59:37 +0300

    Task041 and Task043 added

---
+++ task041/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+/* 0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3
+ */
+
+using static UserMethods.Method;
+
+Console.WriteLine($"How many number you want entered?");
+int usersLenghtArray = InputIntNumberTryParse();
+while(usersLenghtArray <= 0) // отсеивание значений <=0, пока юзер гнормально не введёт.
+{
+    Console.WriteLine($"Entered unacceptable number. Need number > 0");
+    usersLenghtArray = InputIntNumberTryParse(); 
+}
+
+int[] arrayEnteredUsersNumbers = CreatedArrayUserEntered(usersLenghtArray);
+int counterEnteredZero = CounterUserEnteredZero(arrayEnteredUsersNumbers);
+
+Console.WriteLine($"{counterEnteredZero} -- ammount numbers = 0");
+
+
+// метод создание массива путём ввода с клавиатуры
+int[] CreatedArrayUserEntered (int enteredNumber)
+{
+    int[] array = new int[enteredNumber];
+    for (int i = 0; i < array.Length; i++)
+    {   
+        Console.Write($"Enter [{i+1}] number: "); 
+        array[i] = InputIntNumberTryParse();
+    }
+    return array;
+}
+// метод счётчика количество элементов массива с значением 0
+int CounterUserEnteredZero(int[] enteredArray)
+{
+    int count = 0;
+    for(int i = 0; i < enteredArray.Length; i++)
+    {
+        if (enteredArray[i] == 0) count++;
+    }
+    return count;
+}
---
+++ task043/Program.cs
@@ -0,0 +1,25 @@
+﻿/* Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+значения b1, k1, b2 и k2 задаются пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5) */
+
+using static UserMethods.Method;
+
+Console.WriteLine("y = k1 * x + b1, y = k2 * x + b2;");
+
+Console.Write("Enter k1: ");
+double k1 = InputDoubleNumberTryParse();
+Console.Write("Enter b1: ");
+double b1 = InputDoubleNumberTryParse();
+Console.Write("Enter k2: ");
+double k2 = InputDoubleNumberTryParse();
+Console.Write("Enter b2: ");
+double b2 = InputDoubleNumberTryParse();
+Console.WriteLine($"y = {k1} * x + {b1}, y = {k2} * x + {b2};");
+
+Console.WriteLine("");
+double x = (b2 - b1) / (k1 - k2);
+double y = k2 * x + b2;
+Console.WriteLine($"(x, y) = ({x}, {y})");
+
+
